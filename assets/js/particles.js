7d8ac7068bc8fc4c78f63d25d2472afa07d94262
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2018-12-14 15:57:43 +0100

    customize particles

---
+++ assets/js/particles.js
@@ -0,0 +1,121 @@
+
+particlesJS("particles-js", {
+
+  "particles": {
+    "number": {
+      "value": 250,
+      "density": {
+        "enable": true,
+        "value_area": 1500
+      }
+    },
+    "color": {
+      "value": "#555"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "http://image.ibb.co/g9eFcF/logo_transparent.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.6,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 120,
+      "color": "#ffffff",
+      "opacity": 0.1,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.8,
+      "direction": "random",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": true,
+      "attract": {
+        "enable": false,
+        "rotateX": 3600,
+        "rotateY": 3600
+      }
+    }
+  },
+
+
+
+  "interactivity": {
+    "detect_on": "canvas",
+
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+
+      "onclick": {
+        "enable": true,
+        "mode": "remove"
+      },
+      "resize": true
+    },
+
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 100,
+        "size": 4,
+        "duration": 2,
+        "opacity": 1,
+        "speed": 3
+      },
+
+      "repulse": {
+        "distance": 200,
+        "duration": 0.5
+      },
+
+      "push": {
+        "particles_nb": 5
+      },
+
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
