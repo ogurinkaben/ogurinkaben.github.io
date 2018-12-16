da5e35553e950c79e70dde922675b1059b0983c5
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2018-12-17 00:23:57 +0100

    Create type.js

---
+++ assets/js/type.js
@@ -0,0 +1,56 @@
+var TypeString = function(el, Typing, period) {
+  this.Typing = Typing;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TypeString.prototype.tick = function() {
+  var i = this.loopNum % this.Typing.length;
+  var fullTxt = this.Typing[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('type');
+  for (var i=0; i<elements.length; i++) {
+    var Typing = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (Typing) {
+      new TypeString(elements[i], JSON.parse(Typing), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".type > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
