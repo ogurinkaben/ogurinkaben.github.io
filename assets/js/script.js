af355a7f3fd8db5b663600581be69a4fd2435ee1
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2018-12-25 02:20:45 +0100

    Update preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,131 +34,9 @@ const app = new Vue({
     },
   }
 });
-//particles.js config
-particlesJS("particles-js", {
+//===========> Vanilla JS <==============
 
-  "particles": {
-    "number": {
-      "value": 150,
-      "density": {
-        "enable": true,
-        "value_area": 1500
-      }
-    },
-    "color": {
-      "value": "#C0C0C0"
-    },
-    "shape": {
-      "type": ["circle", "triangle", "polygon"],
-      "stroke": {
-        "width": 0,
-        "color": "#0198E1"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "https://res.cloudinary.com/simplytammy/image/upload/v1544838825/hero-background.png",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 1,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.6,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 120,
-      "color": "#ffffff",
-      "opacity": 0.1,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 0.8,
-      "direction": "random",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": true,
-      "attract": {
-        "enable": false,
-        "rotateX": 3600,
-        "rotateY": 3600
-      }
-    }
-  },
-
-
-
-  "interactivity": {
-    "detect_on": "canvas",
-
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-
-      "onclick": {
-        "enable": true,
-        "mode": "remove"
-      },
-      "resize": true
-    },
-
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 100,
-        "size": 4,
-        "duration": 2,
-        "opacity": 1,
-        "speed": 3
-      },
-
-      "repulse": {
-        "distance": 200,
-        "duration": 0.5
-      },
-
-      "push": {
-        "particles_nb": 5
-      },
-
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-
-////===========> Vanilla JS <==============
-
-// Testimonial carousel
+// variables
 let testim = document.getElementById("testim"),
     testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
     testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
@@ -174,7 +52,21 @@ let testim = document.getElementById("testim"),
     ignoreTouch = 30;
 ;
 
-window.onload = function() {
+window.onload = function () {
+  //preloader
+  var delayLoad;
+
+  function preLoad() {
+    delayLoad = setTimeout(showPage, 4000);
+  }
+  preLoad();
+  function showPage() {
+        document.getElementById("vueInit").style.display = "block";
+    document.querySelector('body').style.backgroundColor = "#FFFFFF";
+    document.getElementById("loader").style.display = "none";
+
+  }
+  //Testimonial
     function playSlide(slide) {
         for (let k = 0; k < testimDots.length; k++) {
             testimContent[k].classList.remove("active");
@@ -266,6 +158,7 @@ window.onload = function() {
 }
 
 
+
 //============> jQuery <==============
 
 $(document).ready(function() {
@@ -293,13 +186,7 @@ $(window).scroll(function() {
 //Tooltip init
   $('[data-toggle="tooltip"]').tooltip();
   
-   //makes sure the whole site is loaded
-   $(window).on('load', function () {
-     // will first fade out the loading animation
-     $("#object").fadeOut();
-     // will fade out the whole DIV that covers the website.
-     $("#loading").delay(100).fadeOut("slow");
-   })
+  
 });
 
 
