ea5066fccaf9e47f4b9b1830547dc0fdd1f91e6b
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2019-02-28 11:15:56 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,51 @@
-//===========> Vanilla JS <============== 
-window.onload = function () {
-  //preloader
-  var delayLoad;
+// window.onload = function() {
+// 		var delayLoad;
+// 		var loader = document.getElementById('myloader');
 
-  function preLoad() {
-    delayLoad = setTimeout(showPage, 1000);
-  }
-  preLoad();
-  function showPage() {
-        document.getElementById("vueInit").style.display = "block";
-    document.querySelector('body').style.backgroundColor = "#FFFFFF";
-    document.getElementById("loader").style.display = "none";
+// 		function slowLoad() {
+// 			delayLoad = setTimeout(removeLoader, 1000);
+// 		}
+// 		slowLoad();
 
-  }
+// 		function removeLoader() {
+// 			loader.style.display = 'none';
+// 		}
 
-}
-
-// Animations init
+// 	}
+	// Animations init
 wow = new WOW({
-  boxClass: 'wow', // default
-  animateClass: 'wow', // default
-  offset: 0, // default
-  mobile: true, // default
-  live: true // default
+	boxClass: 'wow', // default
+	animateClass: 'wow', // default
+	offset: 0, // default
+	mobile: true, // default
+	live: true // default
 })
 wow.init();
 //============> jQuery <==============
 
 $(document).ready(function() {
-  // Smooth scrolling
-  let scrollLink = $('.scroll');
+	// Smooth scrolling
+	let scrollLink = $('.scroll');
 
-  scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000 );
-  });
+	scrollLink.click(function(e) {
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 1000);
+	});
 
-  // Back to top
+	// Back to top
 
-  let btn = $('#top');
+	let btn = $('#top');
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 300) {
+			btn.addClass('show');
+		} else {
+			btn.removeClass('show');
+		}
+	});
+	//Tooltip init
+	$('[data-toggle="tooltip"]').tooltip();
+
 });
-//Tooltip init
-  $('[data-toggle="tooltip"]').tooltip();
-  
-  
-});
-
-
-
