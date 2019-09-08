76f39d6a7740f972ec13c3193f84031b4cf14aff
simplytammy <ogurinkabenjamin@gmail.com>
2019-09-08 09:27:41 +0100

    portfolio update

--- app.js
+++ app.js
@@ -19,3 +19,26 @@ function openNav( ) {
 function closeNav( ) {
 	document.getElementById( "myNav" ).style.width = "0%";
 }
+//<============ Vue.js <============
+const Main = {
+	template: '#main'
+}
+const Page2 = {
+	template: '#page2'
+}
+const routes = [ {
+		path: '/',
+		component: Main
+    },
+	{
+		path: '/page2',
+		component: Page2
+    },
+]
+const router = new VueRouter( {
+	routes
+} );
+const app = new Vue( {
+	router,
+	el: '#app',
+} ).$mount( '#app' );
