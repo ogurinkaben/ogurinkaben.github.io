37abf2f8c39a6d56da0a495728194ae628347f5e
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2020-03-16 06:21:00 +0100

    Update

--- app.js
+++ app.js
@@ -2,57 +2,64 @@
     const toggleIcon = document.getElementById("toggle");
 
     function toggleTheme() {
-     if (theme.classList.contains("dark")) {
-      theme.classList.add("light");
-      theme.classList.remove("dark");
-      toggleIcon.style.opacity = "0.5";
-      localStorage.setItem('theme', theme.className);
-     } else {
-      theme.classList.remove("light");
-      theme.classList.add("dark");
-      toggleIcon.style.opacity = "1";
-      localStorage.setItem('theme', theme.className);
-     }
-     console.log(theme.className);
+      if (theme.classList.contains("dark")) {
+        theme.classList.add("light");
+        theme.classList.remove("dark");
+        toggleIcon.style.opacity = "0.5";
+        localStorage.setItem('theme', theme.className);
+      } else {
+        theme.classList.remove("light");
+        theme.classList.add("dark");
+        toggleIcon.style.opacity = "1";
+        localStorage.setItem('theme', theme.className);
+      }
+      console.log(theme.className);
     }
 
     function loadTheme() {
-     if (localStorage.getItem("theme") === null) {
-      localStorage.setItem('theme', "dark");
-     } else {
-      let currentTheme = localStorage.theme;
-      theme.className = currentTheme;
-     }
+      if (localStorage.getItem("theme") === null) {
+        localStorage.setItem('theme', "dark");
+      } else {
+        let currentTheme = localStorage.theme;
+        theme.className = currentTheme;
+      }
     }
     window.onload = loadTheme;
 
     function openNav() {
-     document.getElementById("myNav").style.width = "100%";
+      document.getElementById("myNav").style.width = "100%";
     }
 
     function closeNav() {
-     document.getElementById("myNav").style.width = "0%";
+      document.getElementById("myNav").style.width = "0%";
     }
     //<============ Vue.js <============
     const Code = {
-     template: '#code'
+      template: '#code'
     }
     const Design = {
-     template: '#design'
+      template: '#design'
+    }
+    const NotFound = {
+      template: '#page-not-found'
     }
     const routes = [{
-      path: '/',
-      component: Code
-     },
-     {
-      path: '/design',
-      component: Design
-     },
+        path: '/',
+        component: Code
+      },
+      {
+        path: '/design',
+        component: Design
+      },
+      {
+        path: '*',
+        component: NotFound
+      },
     ]
     const router = new VueRouter({
-     routes
+      routes
     });
     const app = new Vue({
-     router,
-     el: '#app',
+      router,
+      el: '#app',
     }).$mount('#app');
