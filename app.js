95f12407af5e37357275005d6f4a8192299d7aed
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2020-06-02 02:55:00 +0100

    Custom cursor

--- app.js
+++ app.js
@@ -35,8 +35,7 @@ const Design = {
 const NotFound = {
   template: "#page-not-found"
 };
-const routes = [
-  {
+const routes = [{
     name: "Code",
     path: "/",
     component: Code
@@ -65,7 +64,36 @@ router.afterEach((to, from) => {
   NProgress.done();
 });
 const app = new Vue({
-  data: { loading: false },
+  data: {
+    loading: false,
+    xChild: 0,
+    yChild: 0,
+    xParent: 0,
+    yParent: 0,
+    hover: false,
+    hideCursor: true
+  },
+  mounted() {
+    const moveCursor = e => {
+      this.hideCursor = false;
+      this.xChild = e.clientX ? e.clientX : e.touches[0].clientX;
+      this.yChild = e.clientY ? e.clientY : e.touches[0].clientY;
+      setTimeout(() => {
+        this.xParent =
+          e.clientX - 15 ? e.clientX - 15 : e.touches[0].clientX - 15;
+        this.yParent =
+          e.clientY - 15 ? e.clientY - 15 : e.touches[0].clientY - 15;
+      }, 100);
+    };
+    window.addEventListener("mousemove", moveCursor);
+    window.addEventListener("touchmove", moveCursor);
+    window.addEventListener("touchstart", () => {
+      this.hideCursor = false;
+    });
+    window.addEventListener("touchend", () => {
+      this.hideCursor = true;
+    });
+  },
   router,
   el: "#app"
 }).$mount("#app");
